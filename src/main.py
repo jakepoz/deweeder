04963c1f83a057741b6c1ccb34b2ca5817f1f9af
Jake Poznanski <jakepoz@gmail.com>
2022-03-26 21:13:48 +0000

    Adding basic NTRIP client from github.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-import time
+import os
 
 from .magnetometer import Magnetometer
 from .gps import GPS
+from .ntrip import NtripClient
 
 compass = Magnetometer(i2c_device="/dev/i2c-1", address=0x0C)
 #compass.run()
 
+ntrip = NtripClient(caster="rtgpsout.unavco.org",
+                    user=f"{os.getenv('NTRIP_USER')}:{os.getenv('NTRIP_PASSWORD')}",
+                    verbose=True)
+
+ntrip.readData()                    
+
 gps = GPS(serial_port="/dev/ttyACM0")
-gps.run()
+#gps.run()
