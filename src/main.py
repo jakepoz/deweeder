cdd005aa61b7b212a2b2ceb3915296e616ced0dd
Jake Poznanski <jakepoz@gmail.com>
2022-04-14 20:15:15 +0000

    Running starlette and uvicorn, but need to accept cancellation of robot tasks

--- src/main.py
+++ src/main.py
@@ -7,6 +7,12 @@ from .gps import GPS
 from .ntrip import NtripClient
 from .drive_motor import DriveMotor
 
+
+from uvicorn import Config, Server
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
 logging.basicConfig(level=logging.INFO)
 
 #compass = Magnetometer(i2c_device="/dev/i2c-1", address=0x0C)
@@ -16,15 +22,31 @@ ntrip = NtripClient(caster="rtgpsout.unavco.org",
                 password=os.getenv('NTRIP_PASSWORD'),
                 mountpoint="SEAT_RTCM3")                        
 
-# gps = GPS(serial_port="/dev/ttyACM0", ntrip_client=ntrip)
+gps = GPS(serial_port="/dev/ttyACM0", ntrip_client=ntrip)
 
 drive = DriveMotor(pwm_chip=2, pwm_line=0)
 
-async def main():
-    # await asyncio.gather(gps.run(), 
-    #                      ntrip.run(),
-    #                      compass.run())
-    await asyncio.gather(drive._wear_in_motor())
-    
 
-asyncio.run(main())
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
+
+
+async def robot_main():
+    await asyncio.gather(gps.run(), 
+                         ntrip.run())
+                        # compass.run())
+
+app = Starlette(debug=True, routes=[
+    Route('/', homepage),
+])
+
+
+async def server_main():
+    server = Server(config=Config(app, host='0.0.0.0', port=8000, loop="asyncio" ))
+    await server.serve()
+
+async def main():
+    await asyncio.gather(robot_main(), server_main())
+
+if __name__ == "__main__":
+    asyncio.run(main())
