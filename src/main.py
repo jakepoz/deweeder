a183fed2f7bef46849861309d7f012d03d53bad1
Jake Poznanski <jakepoz@gmail.com>
2022-05-20 17:53:14 +0000

    Steer motor works

--- src/main.py
+++ src/main.py
@@ -6,24 +6,25 @@ from .magnetometer import Magnetometer
 from .gps import GPS
 from .ntrip import NtripClient
 from .drive_motor import DriveMotor
+from .steer_motor import SteeringMotor
 from .web import webapp
 
 from uvicorn import Config, Server
 
 logging.basicConfig(level=logging.INFO)
 
-
 #compass = Magnetometer(i2c_device="/dev/i2c-1", address=0x0C)
 
-gps = GPS(serial_port="/dev/ttyACM0")
+#gps = GPS(serial_port="/dev/ttyACM0")
 
-ntrip = NtripClient(caster="rtgpsout.unavco.org",
-                    user=os.getenv('NTRIP_USER'),
-                    password=os.getenv('NTRIP_PASSWORD'),
-                    mountpoint="SEAT_RTCM3",
-                    gps=gps)                        
+#ntrip = NtripClient(caster="rtgpsout.unavco.org",
+#                    user=os.getenv('NTRIP_USER'),
+#                    password=os.getenv('NTRIP_PASSWORD'),
+#                    mountpoint="SEAT_RTCM3",
+#                    gps=gps)                        
 
 drive = DriveMotor(pwm_chip=2, pwm_line=0)
+steer = SteeringMotor(pwm_chip=0, pwm_line=0, gpio_enable_chip="/dev/gpiochip0", gpio_enable_line=8)
 
 server = Server(config=Config(webapp, host='0.0.0.0', port=8000, loop="asyncio"))
 
@@ -32,7 +33,7 @@ async def robot_main(*modules):
     await asyncio.wait([module.run() for module in modules], return_when=asyncio.FIRST_EXCEPTION)
 
 async def main():
-    await asyncio.wait([robot_main(ntrip, gps), server.serve()], return_when=asyncio.FIRST_COMPLETED)
+    await asyncio.wait([robot_main(steer), server.serve()], return_when=asyncio.FIRST_COMPLETED)
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ src/steer_motor.py
@@ -0,0 +1,35 @@
+import asyncio
+import logging
+import numpy as np
+import random
+
+from periphery import PWM, GPIO
+
+
+logger = logging.getLogger(__name__)
+
+PWM_PERIOD = 1/50.0 #20 ms
+PWM_ZERO = .0015
+PWM_MIN = .0010
+PWM_MAX = .0020
+
+class SteeringMotor:
+    def __init__(self, pwm_chip=0, pwm_line=0, gpio_enable_chip="/dev/gpiochip0", gpio_enable_line=8) -> None:
+        self.pwm_chip = pwm_chip
+        self.pwm_line = pwm_line
+        self.pwm = PWM(pwm_chip, pwm_line)
+        self.gpio_enable = GPIO(gpio_enable_chip, gpio_enable_line, "out")
+        self.setup()
+
+    async def setup(self):
+       self.pwm.frequency = 1 / PWM_PERIOD
+       self.pwm.duty_cycle = PWM_ZERO / PWM_PERIOD
+       self.pwm.enable()
+
+       self.gpio_enable.write(True)
+
+    async def run(self):
+        while True:
+            await asyncio.sleep(1)
+            target_duty = random.uniform(PWM_MIN / PWM_PERIOD, PWM_MAX / PWM_PERIOD)
+            self.pwm.duty_cycle = target_duty
